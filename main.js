c9c69393f19e0f39c33002081ccf63b1b2cf2b9a
Seungjoo Kim <sjskybone@gmail.com>
2021-02-12 14:40:20 -0800

    SJ's turn finished.

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 var $entryButton = document.querySelector('.add-button');
 var $addEntrySwitch = document.querySelector('.background-off');
-var $dayOfWeek = document.querySelector('.day-of-week');
-var
 
 function addEntry(event){
   $addEntrySwitch.className = "background";
-
 }
 
 $entryButton.addEventListener("click", addEntry);
+
+var $submitBtn = document.querySelector('.submit-btn');
+var $form = document.querySelector('form');
+
+function submitEntry(event) {
+  event.preventDefault();
+  var newEntry = {};
+  // newEntry.time
+  console.log($form.elements.dayofweek.value);
+
+}
+
+$submitBtn.addEventListener("click", submitEntry);
